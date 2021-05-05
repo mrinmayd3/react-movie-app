d6c5ed68117d69be220ef042b2ec7b20dc3fdcbc
Mrinmay <mrinmaydey48@gmail.com>
2021-05-05 16:12:03 +0530

    Updates

---
+++ precache-manifest.014afb3b5eed4db5d4a39fa0fec506c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b367f3cf08352aea5b137257c5dbb54b",
+    "url": "/REACT_APP_SEARCH_API=https://api.themoviedb.org/3/search/movieindex.html"
+  },
+  {
+    "revision": "f3213a4529675be6f87e",
+    "url": "/REACT_APP_SEARCH_API=https://api.themoviedb.org/3/search/moviestatic/css/main.cc326935.chunk.css"
+  },
+  {
+    "revision": "4b31c7352ce68aa34afa",
+    "url": "/REACT_APP_SEARCH_API=https://api.themoviedb.org/3/search/moviestatic/js/2.b9aa77b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT_APP_SEARCH_API=https://api.themoviedb.org/3/search/moviestatic/js/2.b9aa77b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3213a4529675be6f87e",
+    "url": "/REACT_APP_SEARCH_API=https://api.themoviedb.org/3/search/moviestatic/js/main.eaf3be57.chunk.js"
+  },
+  {
+    "revision": "06c45c4bf36c502e1b76",
+    "url": "/REACT_APP_SEARCH_API=https://api.themoviedb.org/3/search/moviestatic/js/runtime-main.540f3d19.js"
+  }
+]);
